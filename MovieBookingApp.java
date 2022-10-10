cc300d31339b1f557037b0247c2d0807152a2d85
Zisuan <zisuanlim@gmail.com>
2022-10-11 02:28:57 +0800

    Update MovieBookingApp.java

--- MovieBookingApp.java
+++ MovieBookingApp.java
@@ -25,6 +25,84 @@ public class MovieBookingApp {
             System.out.println("8. Update Cinema");
             System.out.println("9. Remove Cinema");
             System.out.println("10. Configure System settings");
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    staff.addMovie();
+                    break;
+                case 2:
+                    staff.updateMovie();
+                    break;
+                case 3:
+                    staff.removeMovie();
+                    break;
+                case 4:
+                    staff.addShowtime();
+                    break;
+                case 5:
+                    staff.updateShowtime();
+                    break;
+                case 6:
+                    staff.removeShowtime();
+                    break;
+                case 7:
+                    staff.addCinema();
+                    break;
+                case 8:
+                    staff.updateCinema();
+                    break;
+                case 9:
+                    staff.removeCinema();
+                    break;
+                case 10:
+                    staff.configureSystemSettings();
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+            
+        }
+        else if (option == 2) {
+            Customer customer = new Customer(null, null, false, null, null, null, null, null);
+            System.out.println("Welcome to Customer Menu");
+            System.out.println("1. Search Movie");
+            System.out.println("2. View Movie Details");
+            System.out.println("3. Check Seat Availability");
+            System.out.println("4. Book Ticket");
+            System.out.println("5. View Booking History");
+            System.out.println("6.List the Top 5 ranking movie");
+            option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    customer.searchMovie();
+                    break;
+                case 2:
+                    customer.viewMovieDetails();
+                    break;
+                case 3:
+                    customer.checkSeatAvailability();
+                    break;
+                case 4:
+                    customer.bookTicket();
+                    break;
+                case 5:
+                    customer.viewBookingHistory();
+                    break;
+                case 6:
+                    customer.listTop5RankingMovie();
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        }
+        else if (option == 3) {
+            System.out.println("Thank you for using our app!");
+            System.exit(0);
+        }
+        else {
+            System.out.println("Invalid option");
         }
 
     }
