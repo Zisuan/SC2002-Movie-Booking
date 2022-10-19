e469c736e6a10dbadc613488b013d07bc744940d
AKASH <44565290+akasht81@users.noreply.github.com>
2022-10-20 01:02:19 +0800

    editted the view modals

---
+++ View/CustomerMenu.java
@@ -0,0 +1,50 @@
+package View;
+import java.util.*;
+
+public class CustomerMenu {
+
+public static void MovieGoerMenu() {
+
+    Scanner sc = new Scanner(System.in);
+    int choice;
+    System.out.println("Welcome to Customer Menu");
+    System.out.println("1. Search/List Movie");
+    System.out.println("2. View Movie Details");
+    System.out.println("3. Check Seat Availability and selection of Seats");
+    System.out.println("4. Book & Purchase Tickets");
+    System.out.println("5. View Booking History");
+    System.out.println("6. List the Top 5 ranking movie by ticket sales OR by overall ratings");
+    System.out.println("7. Exit");
+    choice = sc.nextInt();
+
+    switch (choice) {
+     case 1:
+    // searchMovie();
+    // break;
+    // case 2:
+    // viewMovieDetails();
+    // break;
+    // case 3:
+    // checkSeatAvailability();
+    // break;
+    // case 4:
+    // bookTicket();
+    // break;
+    // case 5:
+    // viewBookingHistory();
+    // break;
+    // case 6:
+    // listTop5RankingMovie();
+    // break;
+    // case 7:
+	//System.out.println("Goodbye!");
+    //break;
+    // default:
+    // System.out.println("Invalid option");
+    // System.out.println("Please re-enter!");
+    // break;
+}
+
+}
+
+}
